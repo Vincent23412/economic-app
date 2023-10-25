6f93f5205c9408daaf5c686ee965b84b187b4705
Vincent23412 <asd1111zxcv@gmail.com>
2023-10-25 16:17:11 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ const outDir = resolve(__dirname, 'docs')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/economic app/',
+  base: '/economic-app/',
   build: {
     outDir,
     emptyOutDir: true
